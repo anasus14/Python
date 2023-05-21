df280ab774b033ae6c1d0c1312ac243948fd52f9
Susana Vega <129547350+anasus14@user.noreply.github.com>
2023-05-21 18:17:39 -0400

    commit

---
+++ Codigos/Codigos.py
@@ -0,0 +1,4 @@
+print("Buenos dias") #Sirve para escribir una indicacion o mostrar resultados
+int() #sirve para que tome la entrada como numero entero
+input("¿cuantos años tienes?") #sirve para obtener una respuesta
+float()#sirve para numeros decimales
