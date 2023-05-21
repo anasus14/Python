7c492f66349c84c53e498f85497900372eded443
Susana Vega <129547350+anasus14@user.noreply.github.com>
2023-05-21 19:20:51 -0400

    commit

--- Codigos/Codigos.py
+++ Codigos/Codigos.py
@@ -5,7 +5,7 @@ float()#sirve para numeros decimales
 int(input()) #se convierte el valor introducido a un numero entero
 try:() #bloque con sentencias que se quieren ejecutar pero podria tener errores
 except:()#se ejecuta cuando try falle con el error
-else:() #se ejecuta si try no tiene errores
+else:() #es una parte falsa
 finally:() #siempre se ejecuta cuando se quiere liberar recursos.
 str() #convierte los numeros en cadena
-
+if 
