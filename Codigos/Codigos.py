650e3b912e3d02cb1a3f3986674e4fc0bba076ff
Susana Vega <129547350+anasus14@user.noreply.github.com>
2023-05-21 18:52:04 -0400

    commit

--- Codigos/Codigos.py
+++ Codigos/Codigos.py
@@ -2,3 +2,10 @@ print("Buenos dias") #Sirve para escribir una indicacion o mostrar resultados
 int() #sirve para que tome la entrada como numero entero
 input("¿cuantos años tienes?") #sirve para obtener una respuesta
 float()#sirve para numeros decimales
+int(input()) #se convierte el valor introducido a un numero entero
+try:() #bloque con sentencias que se quieren ejecutar pero podria tener errores
+except:()#se ejecuta cuando try falle con el error
+else:() #se ejecuta si try no tiene errores
+finally:() #siempre se ejecuta cuando se quiere liberar recursos.
+str() #convierte los numeros en cadena
+
