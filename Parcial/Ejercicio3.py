a6eadafa429ac5f7f15b4561f7d37749a81eebcd
Susana Vega <129547350+anasus14@user.noreply.github.com>
2023-05-31 15:34:32 -0400

    commit

---
+++ Parcial/Ejercicio3.py
@@ -0,0 +1,11 @@
+Set1={100, 250, 300, 1000}
+Set2= {150, 250, 500, 1000}
+#a) Verificar si el valor 100 esta en ambos sets
+
+#b) Comprobar si al menos el valor 500 o 700 estan en alguno de los set
+
+#c) Elevar a 3 todos los valores dentro de los sets
+
+#d) Unir ambos sets en uno solo
+for set1 in Set2:
+    print(set1)
---
+++ Parcial/Parcial.py
@@ -0,0 +1,8 @@
+#Construir un algoritmo que permita generar enteros de 3 en 3 comenzado por el 2 hasta el valor máximo
+# que será menor que 30. Calcular la suma de los enteros generados que sean divisibles por 5, y la suma 
+#de los enteros generados que sean impares.
+Maximo=30
+num=2
+while num < Maximo:
+    #divisibles 5
+    num % 5
