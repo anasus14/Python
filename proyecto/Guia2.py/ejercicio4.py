120bdca2ab8f917e33a74397039fe06fc89311ce
Susana Vega <129547350+anasus14@user.noreply.github.com>
2023-06-07 21:58:58 -0400

    commit

---
+++ proyecto/Guia2.py/ejercicio4.py
@@ -0,0 +1,7 @@
+def identificacion():
+    usuario=input("Ingrese su nombre de usuario: ")
+    return usuario
+def contraseña():
+    contra=input("Ingrese su contraseña de 4 digitos: ")
+    return contra
+
